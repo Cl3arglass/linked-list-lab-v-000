42b8a334627511d7044e9398aa34d4066a25e4f9
Holden Altaffer <haltaffer@gmail.com>
2019-10-11 23:29:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ function next(node, collection){
   let nextAddress = node.next
   return collection[`${nextAddress}`]
 }
+
+function nodeAt(index, linkedList, collection) {
+  let currentNode = headNode(linkedList, collection);
+  for(let i = 0; i < index; i++){
+     currentNode = next(currentNode, collection);
+  }
+
+  return currentNode;
+}
